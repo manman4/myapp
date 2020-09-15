c9935b2953de25af4063d55f7c7d054cf4daeb7f
manman4 <manchanr4@gmail.com>
2020-09-15 11:54:33 +0900

    main.py作成

--- flask_app/main.py
+++ flask_app/main.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 
 # / へのアクセスの処理
 @app.route('/')
-def hello():
-    name = "Hello World"
-    return name
+def show_result():
+    with open("test.txt", "r") as f:
+        data_list = []
+        for i in f:
+          data_list.append(i.rstrip("\n"))
 
-@app.route('/good')
-def good():
-    name = "Good"
-    return name
+        start_time = data_list[0]
+        end_time = data_list[1]
+        num = data_list[2]
 
-## おまじない
+    return render_template("result.html", start_time=start_time,
+                            end_time=end_time, num=num)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='127.0.0.1', port=5000)
