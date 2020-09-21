2635c68ae3659e11993e6d7286518b6a6da81e56
manman4 <manchanr4@gmail.com>
2020-09-21 14:57:33 +0900

    所在地を外だし

--- make_map/make_map.py
+++ make_map/make_map.py
@@ -1,15 +1,12 @@
 import pandas as pd
 import folium
 
-# データフレームを読み込む
 df = pd.read_csv("prefecture.csv")
-
-# 店舗のある地点（緯度、経度）と店舗名をリスト化する
-store = df[["緯度","経度","所在地"]].values
+prefectures = df[["緯度","経度","所在地"]].values
 
 # 地図を作って書き出す
 m = folium.Map(location=[35.68944,135], zoom_start=5.5)
-for data in store:
+for data in prefectures :
   	folium.Marker([data[0], data[1]], tooltip=data[2]).add_to(m)
 
 m.save('prefecture.html')
